3a8776caebdebe1e62d9eb51ec62b6d4bed10597
DiegoooLoomans <513642@vistacollege.nl>
2022-11-17 12:39:48 +0100

    commit
    commit

---
+++ rijbewijs.py
@@ -0,0 +1,8 @@
+leeftijd = int(input("Hoe oud ben je?:"))
+
+if (leeftijd < 18):
+    print("Je bent nog niet oud genoeg om te rijden!")
+
+
+if (leeftijd == 18) or leeftijd > 18:
+    print("Je bent oud genoeg om te mogen rijden!")
